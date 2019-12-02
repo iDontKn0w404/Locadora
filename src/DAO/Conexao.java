aeee925c9272434ecedb1075593cffea14b4e681
Wilde <44567489+iDontKn0w404@users.noreply.github.com>
2019-12-02 08:48:18 -0300

    Adcionei a biblioteca de conector MySQL e criei a classe Conexao.java da classe DAO.

--- src/DAO/Conexao.java
+++ src/DAO/Conexao.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DAO;
 
-/**
- *
- * @author Will
- */
+import java.sql.*;
+import javax.swing.JOptionPane;
+//import Connection;
+
 public class Conexao {
     
+    public static Connection AbrirConexao(){
+        Connection con = null;
+        try {
+            Class.forName("com.mysqk.jdbc.Driver");
+            String url = "jdbc:mysql://localhost/locadora";
+            con = DriverManager.getConnection(url, "root", "123");
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Erro na Conexão com o Banco", 
+                    "Vídeo Locadora", JOptionPane.ERROR_MESSAGE);
+            e.getMessage();
+        }
+        return con;
+    }
+    
+    public static void FecharConexao(Connection con) {
+        try {
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
