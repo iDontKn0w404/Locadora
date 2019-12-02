462a384cd54bf0e59ec5d501796bcdd7a3ac8770
Wilde <44567489+iDontKn0w404@users.noreply.github.com>
2019-12-02 09:38:51 -0300

    Criei o metodo Logar() em FuncionarioDAO.java. E adicionei os atributos e métodos set e get da classe Modelo.

--- src/Modelo/Categoria.java
+++ src/Modelo/Categoria.java
@@ -6,4 +6,23 @@ public class Categoria {
     private int codigo;
     private String nome;
     
+    //metodos getters e setters
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
 }
